88ff46417aad25c176a522d49c0576c068c5925b
Mike <agyemangmichael555@gmail.com>
2025-03-31 01:14:14 +0000

    done for the day

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingsComponent } from '../../components/greetings/greetings.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   title = 'Angular Tutorials by AHSAN!';
+  mySignal = signal('first signal instance bro!');
+  homeMessage = signal('Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!');
 }
