ce84de24b71fdca47f44c5dece6ef9a2f7b4d86a
Mike <agyemangmichael555@gmail.com>
2025-04-01 02:49:27 +0000

    done with this course bro!

--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
+import { Todos } from '../../models/todo.type';
+import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
+  providers: [TodosService],
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todos>>([]);
 
+  ngOnInit(): void {
+    this.todoService
+      .getTodosFromAPI()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+          // throw new Error('failed to fetch todos...');
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+
+    // console.log(this.todoServices.todoItems);
+    // this.todoItems.set(this.todoServices.todoItems);
+  }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Todos } from '../models/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
+  http = inject(HttpClient);
+  // todoItems: Array<Todos> = [
+  //   {
+  //     id: 0,
+  //     userId: 1,
+  //     title: 'Buy provisions',
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 1,
+  //     userId: 1,
+  //     title: 'Trim hair',
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     userId: 1,
+  //     title: 'Go for food from Granny',
+  //     completed: true,
+  //   },
+  //   {
+  //     id: 0,
+  //     userId: 1,
+  //     title: 'Finish with tutorial 2!',
+  //     completed: false,
+  //   },
+  // ];
+  //constructor() { }
 
-  constructor() { }
+  getTodosFromAPI() {
+    const APIurl = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get<Array<Todos>>(APIurl);
+  }
 }
